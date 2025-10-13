bf486b6c7593a38bc4a3a8613580a8606d15507c
quentin-fouilleul <fouilleul.quentin@outlook.fr>
2025-10-13 21:39:47 +0200

    Initial commit

---
+++ src/main/java/fr/quentin/bluebell/Bluebell.java
@@ -0,0 +1,17 @@
+package fr.quentin.bluebell;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Bluebell implements ModInitializer {
+	public static final String MOD_ID = "bluebell";
+	public static final String MOD_NAME = "Bluebell";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/fr/quentin/bluebell/BluebellClient.java
@@ -0,0 +1,10 @@
+package fr.quentin.bluebell;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BluebellClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
