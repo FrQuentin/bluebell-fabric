14febcc88f02f731a8540c2b8520c6573bf8ce87
quentin-fouilleul <fouilleul.quentin@outlook.fr>
2025-10-13 21:48:56 +0200

    0.01.0

--- src/main/java/fr/quentin/bluebell/BluebellDataGenerator.java
+++ src/main/java/fr/quentin/bluebell/BluebellDataGenerator.java
@@ -1,11 +1,20 @@
 package fr.quentin.bluebell;
 
+import fr.quentin.bluebell.datagen.ModItemTagProvider;
+import fr.quentin.bluebell.datagen.ModModelProvider;
+import fr.quentin.bluebell.datagen.ModRecipeProvider;
+import fr.quentin.bluebell.datagen.language.ModEnglishLanguageProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class BluebellDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+        pack.addProvider(ModItemTagProvider::new);
+        pack.addProvider(ModModelProvider::new);
+        pack.addProvider(ModRecipeProvider::new);
+        pack.addProvider(ModEnglishLanguageProvider::new);
 	}
 }
